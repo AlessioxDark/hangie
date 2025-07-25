6807d9715ee16765ecb3c1714c4564afa121b391
Alessio40 <alessio40aq@gmail.com>
2025-07-25 18:33:12 +0200

    testando deployment

--- frontend/hangie-frontend/src/app/App.tsx
+++ frontend/hangie-frontend/src/app/App.tsx
@@ -3,7 +3,13 @@ import { useState } from 'react';
 function App() {
 	const [count, setCount] = useState(0);
 
-	return <></>;
+	return (
+		<>
+			<div>
+				<h1>ciao</h1>
+			</div>
+		</>
+	);
 }
 
 export default App;

298aee70f16a75cbf26bdd50fef5894fc7d4f1b3
Alessio40 <alessio40aq@gmail.com>
2025-07-25 18:41:55 +0200

    provo main usestat errore

--- frontend/hangie-frontend/src/app/App.tsx
+++ frontend/hangie-frontend/src/app/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	return <></>;
+	return (
+		<div>
+			<h1>ciao tutto apposto</h1>
+		</div>
+	);
 }
 
 export default App;

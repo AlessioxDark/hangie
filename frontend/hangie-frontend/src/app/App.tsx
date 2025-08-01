d7b5925691b9418d94f756c4b15acafcf778a694
Alessio40 <alessio40aq@gmail.com>
2025-08-01 19:01:41 +0200

    proviamo request

--- frontend/hangie-frontend/src/app/App.tsx
+++ frontend/hangie-frontend/src/app/App.tsx
@@ -1,4 +1,11 @@
+import { useEffect } from 'react';
+
 function App() {
+	useEffect(() => {
+		fetch('localhost:3000/api/events/discover')
+			.then((res) => res.json())
+			.then((data) => console.log(data));
+	});
 	return (
 		<div>
 			<h1>Menomale che funziona</h1>
